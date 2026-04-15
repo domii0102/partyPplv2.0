0f6b40ee7b318853f89a16fe528cd2023566046a
domii0102 <d.zubrycka4@gmail.com>
2026-04-15 18:38:55 +0200

    Naprawienie logowania, zdjęcie w ProfileView zaciągane z userStore

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,104 +1,119 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import { useUserStore } from '../stores/user';
+import { createRouter, createWebHistory } from "vue-router";
+import { useUserStore } from "../stores/user";
 
 // import widoków
-import FillInProfileInfoView from '../views/Account/FillInProfileInfoView.vue'
-import ForgotPasswordView from '../views/Account/ForgotPasswordView.vue'
-import LoginView from '../views/Account/LoginView.vue'
-import RegisterView from '../views/Account/RegisterView.vue'
-import ResetPasswordView from '../views/Account/ResetPasswordView.vue'
-import VerifyEmailView from '../views/Account/VerifyEmailView.vue'
-import EnterEmailView from '../views/Account/EnterEmailView.vue'
-import CreateEventView from '../views/Event/CreateEventView.vue'
-import EventDashboardView from '../views/Event/EventDashboardView.vue'
-import EventFeedView from '../views/Event/EventFeedView.vue'
+import FillInProfileInfoView from "../views/Account/FillInProfileInfoView.vue";
+import ForgotPasswordView from "../views/Account/ForgotPasswordView.vue";
+import LoginView from "../views/Account/LoginView.vue";
+import RegisterView from "../views/Account/RegisterView.vue";
+import ResetPasswordView from "../views/Account/ResetPasswordView.vue";
+import VerifyEmailView from "../views/Account/VerifyEmailView.vue";
+import EnterEmailView from "../views/Account/EnterEmailView.vue";
+import CreateEventView from "../views/Event/CreateEventView.vue";
+import EventDashboardView from "../views/Event/EventDashboardView.vue";
+import EventFeedView from "../views/Event/EventFeedView.vue";
 
-import LandingPageView from '../views/Home/LandingPageView.vue'
+import LandingPageView from "../views/Home/LandingPageView.vue";
 
-import ProfileView from '../views/Profile/ProfileView.vue'
-import NotificationsView from '../views/Profile/NotificationsView.vue';
+import ProfileView from "../views/Profile/ProfileView.vue";
+import NotificationsView from "../views/Profile/NotificationsView.vue";
 
-import { getActivePinia, setActivePinia, createPinia } from 'pinia'
+import { getActivePinia, setActivePinia, createPinia } from "pinia";
 
-const pinia = getActivePinia() || createPinia()
-setActivePinia(pinia)
+const pinia = getActivePinia() || createPinia();
+setActivePinia(pinia);
 
 //const store = useUserStore();
 
 const routes = [
-  { path: '/', component: LandingPageView },
+  { path: "/", component: LandingPageView },
 
   // Auth
-  { path: '/login', component: LoginView, meta: { hideHeader: true } },
-  { path: '/register', component: RegisterView, meta: { hideHeader: true } },
-  { path: '/forgot-password', component: ForgotPasswordView, meta: { hideHeader: true } },
-  { path: '/reset-password', component: ResetPasswordView, meta: { hideHeader: true } },
-  { path: '/enter-email', component: EnterEmailView, meta: { hideHeader: true }},
-  { path: '/verify-email', component: VerifyEmailView, meta: { hideHeader: true } },
-  { path: '/create', component: FillInProfileInfoView, meta: { hideHeader: true, requiresAuth: true, requiresVerification: false } },
+  { path: "/login", component: LoginView, meta: { hideHeader: true } },
+  { path: "/register", component: RegisterView, meta: { hideHeader: true } },
+  {
+    path: "/forgot-password",
+    component: ForgotPasswordView,
+    meta: { hideHeader: true },
+  },
+  {
+    path: "/reset-password",
+    component: ResetPasswordView,
+    meta: { hideHeader: true },
+  },
+  {
+    path: "/enter-email",
+    component: EnterEmailView,
+    meta: { hideHeader: true },
+  },
+  {
+    path: "/verify-email",
+    component: VerifyEmailView,
+    meta: { hideHeader: true },
+  },
+  {
+    path: "/create",
+    component: FillInProfileInfoView,
+    meta: { hideHeader: true, requiresAuth: false, requiresVerification: true },
+  },
 
   // Event
-  { 
-    path: '/event/create', 
-    component: CreateEventView, 
-    meta: { requiresAuth: true, requiresVerification: true }
+  {
+    path: "/event/create",
+    component: CreateEventView,
+    meta: { requiresAuth: true, requiresVerification: true },
   },
-  { 
-    path: '/event/feed', 
-    component: EventFeedView, 
-    meta: { requiresAuth: true, requiresVerification: true }
+  {
+    path: "/event/feed",
+    component: EventFeedView,
+    meta: { requiresAuth: true, requiresVerification: true },
   },
-  { 
-    path: '/event/dashboard/:id', 
-    component: EventDashboardView, 
-    meta: { requiresAuth: true, requiresVerification: true }
+  {
+    path: "/event/dashboard/:id",
+    component: EventDashboardView,
+    meta: { requiresAuth: true, requiresVerification: true },
   },
 
   // Profile
-  { 
-    path: '/profile', 
-    component: ProfileView, 
-    meta: { requiresAuth: true, requiresVerification: true } 
+  {
+    path: "/profile",
+    component: ProfileView,
+    meta: { requiresAuth: true, requiresVerification: true },
   },
 
-  { 
-    path: '/notifications', 
-    component: NotificationsView, 
-    meta: { requiresAuth: true, requiresVerification: true } 
+  {
+    path: "/notifications",
+    component: NotificationsView,
+    meta: { requiresAuth: true, requiresVerification: true },
   },
-
 ];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  routes,
 });
 
 router.beforeEach(async (to, from, next) => {
-  const store = useUserStore(); 
+  const store = useUserStore();
 
   if (!store.getUser) {
     await store.loadUser();
   }
 
   const isAuthenticated = store.user != null;
-  const isVerified = localStorage.getItem('user_verified') === 'true';
+  const isVerified = localStorage.getItem("user_verified") === "true";
 
   if (to.meta.requiresAuth && !isAuthenticated) {
-    return next('/login');
-  } 
-  
-  else if (to.meta.requiresVerification && !isVerified) { 
-    if (to.path === '/verify-email') {
+    return next("/login");
+  } else if (to.meta.requiresVerification && !isVerified) {
+    if (to.path === "/verify-email") {
       return next();
     } else {
-      return next('/verify-email');
+      return next("/verify-email");
     }
-  } 
-
-  else if (!to.meta.requiresAuth && isAuthenticated) {
-    return next('/profile');
-  } 
+  } else if (!to.meta.requiresAuth && isAuthenticated) {
+    return next("/profile");
+  }
   next();
 });
 
--- client/src/views/Profile/ProfileView.vue
+++ client/src/views/Profile/ProfileView.vue
@@ -1,153 +1,154 @@
 <template>
-    <div class="profile-header">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-md-auto text-center text-md-start mb-3 mb-md-0">
-                    <div class="avatar-container mx-auto mx-md-0">
-                        <img class="avatar-img" :src="defaultImage" alt="Avatar" >
-                    </div>
-                </div>
-                <div class="col-md">
-                    <div class="profile-info text-center text-md-start ms-md-4">
-                        <h1>{{ user?.name }}</h1>
-                        <div class="nickname">{{ user?.nickname }}</div>
-                        <div class="stats">
-                            {{ 67 }} friends
-                        </div>
-                        <a class="btn btn-edit-profile">
-                            Edit profile
-                        </a>
-                        <button class="btn btn-none log-out" @click="logOut">
-                            <i class="bi bi-box-arrow-right"></i>
-                            Log out
-                        </button>
-                    </div>
-                </div>
-            </div>
+  <div class="profile-header">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-md-auto text-center text-md-start mb-3 mb-md-0">
+          <div class="avatar-container mx-auto mx-md-0">
+            <img class="avatar-img" :src="user?.avatar?.url" alt="Avatar" />
+          </div>
         </div>
+        <div class="col-md">
+          <div class="profile-info text-center text-md-start ms-md-4">
+            <h1>{{ user?.name }}</h1>
+            <div class="nickname">{{ user?.nickname }}</div>
+            <div class="stats">{{ 67 }} friends</div>
+            <a class="btn btn-edit-profile"> Edit profile </a>
+            <button class="btn btn-none log-out" @click="logOut">
+              <i class="bi bi-box-arrow-right"></i>
+              Log out
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="gradient-line"></div>
+
+  <div class="container pb-5">
+    <div class="row mb-5">
+      <div
+        class="section-title col-12 d-flex justify-content-between align-items-center"
+      >
+        <div>
+          <h2>Your events</h2>
+          <div class="gradient-line"></div>
+        </div>
+        <div>
+          <FontAwesomeIcon icon="chevron-left" class="icon"></FontAwesomeIcon>
+          <FontAwesomeIcon icon="chevron-right" class="icon"></FontAwesomeIcon>
+        </div>
+      </div>
+
+      <div class="col-12 event-scroll-container">To twoje</div>
     </div>
 
-    <div class="gradient-line"></div>
-
-    <div class="container pb-5">
-        <div class="row mb-5">
-            <div class="section-title col-12 d-flex justify-content-between align-items-center">
-                <div>
-                    <h2>Your events</h2>
-                    <div class="gradient-line"></div>
-                </div>
-                <div>
-                    <FontAwesomeIcon icon="chevron-left" class="icon"></FontAwesomeIcon>
-                    <FontAwesomeIcon icon="chevron-right" class="icon"></FontAwesomeIcon>
-                </div>
-            </div>
-            
-            <div class="col-12 event-scroll-container">
-                To twoje
-            </div>
+    <div class="row">
+      <div
+        class="section-title col-12 d-flex justify-content-between align-items-center"
+      >
+        <div>
+          <h2>Events you're participating in</h2>
+          <div class="gradient-line"></div>
         </div>
-
-        <div class="row">
-            <div class="section-title col-12 d-flex justify-content-between align-items-center">
-                <div>
-                    <h2>Events you're participating in</h2>
-                    <div class="gradient-line"></div>
-                </div>
-                <div>
-                    <FontAwesomeIcon icon="chevron-left" class="icon"></FontAwesomeIcon>
-                    <FontAwesomeIcon icon="chevron-right" class="icon"></FontAwesomeIcon>
-                </div>
-            </div>
-            <div class="col-12 event-scroll-container">
-                Tu bylas i bylo fajnie
-            </div>
+        <div>
+          <FontAwesomeIcon icon="chevron-left" class="icon"></FontAwesomeIcon>
+          <FontAwesomeIcon icon="chevron-right" class="icon"></FontAwesomeIcon>
         </div>
+      </div>
+      <div class="col-12 event-scroll-container">Tu bylas i bylo fajnie</div>
     </div>
+  </div>
 </template>
 
-<style scoped>
-.icon{
-    cursor: pointer;
-    color: var(--text-muted);
-}
-.profile-header{
-    background: linear-gradient(270deg, var(--bg-main), var(--accent-purple), transparent 1000%);
-    padding: 4rem;
-}
-.avatar-container{
-    width: auto;
-    height: auto;
-    max-width: 240px;
-    aspect-ratio: 1/1;
-    border-radius: 20px;
-    overflow: hidden;
-    border: 1px solid var(--accent-purple);
-}
-.avatar-img{
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-}
-.gradient-line{
-    height: 2px;
-    width: 100%;
-    background: var(--line-gradient);
-}
-.profile-info h1{
-    font-size: 3.5rem;
-    font-weight: 300;
-    margin-bottom: 0;
-    color: var(--text-main);
-}
-.profile-info .nickname {
-    color: var(--accent-orange);
-    font-size: 1.2rem;
-    margin-bottom: 1rem;
-    font-style: italic;
-}
-.stats {
-    font-size: 1.1rem;
-    color: var(--text-muted);
-    margin-bottom: 1rem;
-}
-.btn-edit-profile {
-    background: transparent;
-    border: 1px solid var(--accent-orange);
-    color: var(--text-main);
-    padding: 8px 30px;
-    border-radius: 20px;
-    transition: all 0.3s;
-}
-
-.btn-edit-profile:hover {
-    background: var(--accent-orange);
-    text-decoration: none;
-}
-.section-title{
-    margin-top: 2rem;
-}
-.section-title h2{
-    font-size: 1.7rem;
-    font-weight: 400;
-}
-
-.log-out{
-    margin-left: 2rem;
-    color: var(--text-main);
-    text-decoration: none;
-}
-</style>
-
 <script setup>
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import defaultImage from '../../assets/horsegiirl.jpg';
-import { useUserStore } from '../../stores/user.js';
-import { storeToRefs } from 'pinia';
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import defaultImage from "../../assets/horsegiirl.jpg";
+import { useUserStore } from "../../stores/user.js";
+import { storeToRefs } from "pinia";
 
 const store = useUserStore();
 const { user } = storeToRefs(store);
 
 const logOut = async () => {
-    store.logout();
-}
+  store.logout();
+};
 </script>
+
+<style scoped>
+.icon {
+  cursor: pointer;
+  color: var(--text-muted);
+}
+.profile-header {
+  background: linear-gradient(
+    270deg,
+    var(--bg-main),
+    var(--accent-purple),
+    transparent 1000%
+  );
+  padding: 4rem;
+}
+.avatar-container {
+  width: auto;
+  height: auto;
+  max-width: 240px;
+  aspect-ratio: 1/1;
+  border-radius: 20px;
+  overflow: hidden;
+  border: 1px solid var(--accent-purple);
+}
+.avatar-img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+.gradient-line {
+  height: 2px;
+  width: 100%;
+  background: var(--line-gradient);
+}
+.profile-info h1 {
+  font-size: 3.5rem;
+  font-weight: 300;
+  margin-bottom: 0;
+  color: var(--text-main);
+}
+.profile-info .nickname {
+  color: var(--accent-orange);
+  font-size: 1.2rem;
+  margin-bottom: 1rem;
+  font-style: italic;
+}
+.stats {
+  font-size: 1.1rem;
+  color: var(--text-muted);
+  margin-bottom: 1rem;
+}
+.btn-edit-profile {
+  background: transparent;
+  border: 1px solid var(--accent-orange);
+  color: var(--text-main);
+  padding: 8px 30px;
+  border-radius: 20px;
+  transition: all 0.3s;
+}
+
+.btn-edit-profile:hover {
+  background: var(--accent-orange);
+  text-decoration: none;
+}
+.section-title {
+  margin-top: 2rem;
+}
+.section-title h2 {
+  font-size: 1.7rem;
+  font-weight: 400;
+}
+
+.log-out {
+  margin-left: 2rem;
+  color: var(--text-main);
+  text-decoration: none;
+}
+</style>
